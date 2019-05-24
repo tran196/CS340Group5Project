47b1a59310feb0864610cb3af6406e7b2832e80e
Andres Arizala <arizala13@gmail.com>
2019-05-24 15:21:14 -0500

    Add files via upload

--- people.js
+++ people.js
@@ -13,10 +13,10 @@ module.exports = function(){
         });
     }
 
-    /*
+
 
     function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id", function(error, results, fields){
+        mysql.pool.query("SELECT bsg_people.character_id as id, fname, lname, nfl_football_players.name AS homeworld, age FROM bsg_people INNER JOIN nfl_football_players ON homeworld = nfl_football_players.player_id", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -24,7 +24,7 @@ module.exports = function(){
             context.people = results;
             complete();
         });
-    } */
+    } 
 
 /*
     function getPeoplebyHomeworld(req, res, mysql, context, complete){
@@ -79,7 +79,7 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        //getPeople(res, mysql, context, complete);
+        getPeople(res, mysql, context, complete);
         getPlanets(res, mysql, context, complete);
         function complete(){
             callbackCount++;
