9e4575d6e597d2b8b9204078b3fdbdf23c938ae6
Andres Arizala <arizala13@gmail.com>
2019-05-26 23:37:47 -0500

    Add files via upload
    couple updates 
    - ability to add players, works with DB
    - cleaned up interface on a couple pages

--- people_certs.js
+++ people_certs.js
@@ -2,9 +2,21 @@ module.exports = function(){
     var express = require('express');
     var router = express.Router();
 
-    /* get people to populate in dropdown */
+    function getPlanets(res, mysql, context, complete){
+        mysql.pool.query("SELECT player_id, fname, lname, age FROM nfl_football_players", function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.planets  = results;
+            complete();
+        });
+    }
+
+
+
     function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT character_id AS pid, fname, lname FROm bsg_people", function(error, results, fields){
+        mysql.pool.query("SELECT player_id, fname, lname, age FROM nfl_football_players", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -14,100 +26,174 @@ module.exports = function(){
         });
     }
 
-    /* get certificates to populate in dropdown */
-    function getCertificates(res, mysql, context, complete){
-        sql = "SELECT certification_id AS cid, title FROM bsg_cert";
-        mysql.pool.query(sql, function(error, results, fields){
+
+    function getPeoplebyHomeworld(req, res, mysql, context, complete){
+      var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.homeworld = ?";
+      console.log(req.params)
+      var inserts = [req.params.homeworld]
+      mysql.pool.query(query, inserts, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
-                res.end()
+                res.end();
             }
-            context.certificates = results
+            context.people = results;
             complete();
         });
     }
 
-    /* get people with their certificates */
-    /* TODO: get multiple certificates in a single column and group on
-     * fname+lname or id column
-     */
-    function getPeopleWithCertificates(res, mysql, context, complete){
-        sql = "SELECT pid, cid, CONCAT(fname,' ',lname) AS name, title AS certificate FROM bsg_people INNER JOIN bsg_cert_people on bsg_people.character_id = bsg_cert_people.pid INNER JOIN bsg_cert on bsg_cert.certification_id = bsg_cert_people.cid ORDER BY name, certificate"
-         mysql.pool.query(sql, function(error, results, fields){
+    /* Find people whose fname starts with a given string in the req */
+    function getPeopleWithNameLike(req, res, mysql, context, complete) {
+      //sanitize the input as well as include the % character
+       var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.fname LIKE " + mysql.pool.escape(req.params.s + '%');
+      console.log(query)
+
+      mysql.pool.query(query, function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
-                res.end()
+                res.end();
             }
-            context.people_with_certs = results
+            context.people = results;
             complete();
         });
     }
-  
 
-    /* List people with certificates along with 
-     * displaying a form to associate a person with multiple certificates
-     */
+
+
+    function getPerson(res, mysql, context, id, complete){
+        var sql = "SELECT character_id as id, fname, lname, homeworld, age FROM bsg_people WHERE character_id = ?";
+        var inserts = [id];
+        mysql.pool.query(sql, inserts, function(error, results, fields){
+            if(error){
+                res.write(JSON.stringify(error));
+                res.end();
+            }
+            context.person = results[0];
+            complete();
+        });
+    }
+
+    /*Display all people. Requires web based javascript to delete users with AJAX*/
+
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = ["deleteperson.js"];
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
         var mysql = req.app.get('mysql');
-        var handlebars_file = 'people_certs'
-
         getPeople(res, mysql, context, complete);
-        getCertificates(res, mysql, context, complete);
-        getPeopleWithCertificates(res, mysql, context, complete);
+        getPlanets(res, mysql, context, complete);
         function complete(){
             callbackCount++;
-            if(callbackCount >= 3){
-                res.render(handlebars_file, context);
+            if(callbackCount >= 2){
+                res.render('people_certs', context);
+            }
+
+        }
+    });
+
+    // Display all people from a given homeworld. Requires web based javascript to delete users with AJAX
+    router.get('/filter/:homeworld', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        var mysql = req.app.get('mysql');
+        getPeoplebyHomeworld(req,res, mysql, context, complete);
+        getPlanets(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('people_certs', context);
+            }
+
+        }
+    });
+
+
+
+    // Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX
+    router.get('/search/:s', function(req, res){
+        var callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        var mysql = req.app.get('mysql');
+        getPeopleWithNameLike(req, res, mysql, context, complete);
+        getPlanets(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('people_certs', context);
             }
         }
     });
 
-    /* Associate certificate or certificates with a person and 
-     * then redirect to the people_with_certs page after adding 
-     */
-    router.post('/', function(req, res){
-        console.log("We get the multi-select certificate dropdown as ", req.body.certs)
+     // Display one person for the specific purpose of updating people
+
+    router.get('/:id', function(req, res){
+        callbackCount = 0;
+        var context = {};
+        context.jsscripts = ["selectedplanet.js", "updateperson.js"];
         var mysql = req.app.get('mysql');
-        // let's get out the certificates from the array that was submitted by the form 
-        var certificates = req.body.certs
-        var person = req.body.pid
-        for (let cert of certificates) {
-          console.log("Processing certificate id " + cert)
-          var sql = "INSERT INTO bsg_cert_people (pid, cid) VALUES (?,?)";
-          var inserts = [person, cert];
-          sql = mysql.pool.query(sql, inserts, function(error, results, fields){
-            if(error){
-                //TODO: send error messages to frontend as the following doesn't work
-                /* 
-                res.write(JSON.stringify(error));
-                res.end();
-                */
-                console.log(error)
+        getPerson(res, mysql, context, req.params.id, complete);
+        getPlanets(res, mysql, context, complete);
+        function complete(){
+            callbackCount++;
+            if(callbackCount >= 2){
+                res.render('update-person', context);
             }
-          });
-        } //for loop ends here 
-        res.redirect('/people_certs');
+
+        }
     });
 
-    /* Delete a person's certification record */
-    /* This route will accept a HTTP DELETE request in the form
-     * /pid/{{pid}}/cert/{{cid}} -- which is sent by the AJAX form 
-     */
-    router.delete('/pid/:pid/cert/:cid', function(req, res){
-        //console.log(req) //I used this to figure out where did pid and cid go in the request
-        console.log(req.params.pid)
-        console.log(req.params.cid)
+    /* Adds a person, redirects to the people page after adding */
+
+    router.post('/', function(req, res){
+        console.log(req.body.homeworld)
+        console.log(req.body)
         var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM bsg_cert_people WHERE pid = ? AND cid = ?";
-        var inserts = [req.params.pid, req.params.cid];
+        var sql = "INSERT INTO nfl_football_players (player_id, fname, lname, age) VALUES (?,?,?,?)";
+        var inserts = [req.body.player_id, req.body.fname, req.body.lname, req.body.age];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(JSON.stringify(error))
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.redirect('/people_certs');
+            }
+        });
+    });
+
+     // The URI that update data is sent to in order to update a person
+
+    router.put('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        console.log(req.body)
+        console.log(req.params.id)
+        var sql = "UPDATE bsg_people SET fname=?, lname=?, homeworld=?, age=? WHERE character_id=?";
+        var inserts = [req.body.fname, req.body.lname, req.body.homeworld, req.body.age, req.params.id];
+        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+            if(error){
+                console.log(error)
+                res.write(JSON.stringify(error));
+                res.end();
+            }else{
+                res.status(200);
+                res.end();
+            }
+        });
+    });
+
+     // Route to delete a person, simply returns a 202 upon success. Ajax will handle this.
+
+    router.delete('/:id', function(req, res){
+        var mysql = req.app.get('mysql');
+        var sql = "DELETE FROM bsg_people WHERE character_id = ?";
+        var inserts = [req.params.id];
         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
+                console.log(error)
                 res.write(JSON.stringify(error));
-                res.status(400); 
-                res.end(); 
+                res.status(400);
+                res.end();
             }else{
                 res.status(202).end();
             }
