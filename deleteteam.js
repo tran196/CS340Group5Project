23b6992a3ccc6db7cb3a655b7065b1969604600b
Andres Arizala <arizala13@gmail.com>
2019-06-08 23:17:51 -0500

    Add files via upload
    - deletes working 
    - updates giving errors - not sure what it is, maybe the table? I think I changed something on the leagues one to make it work. Not sure.

---
+++ deleteteam.js
@@ -0,0 +1,9 @@
+function deleteteam(id){
+    $.ajax({
+        url: '/people/' + id,
+        type: 'DELETE',
+        success: function(result){
+            window.location.reload(true);
+        }
+    })
+};
--- people.js
+++ people.js
@@ -13,10 +13,8 @@ module.exports = function(){
         });
     }
 
-
-
     function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT fantasy_teams.team_id, league_id, owner_id FROM fantasy_teams", function(error, results, fields){
+        mysql.pool.query("SELECT fantasy_teams.team_id as id, league_id, owner_id FROM fantasy_teams", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -27,35 +25,35 @@ module.exports = function(){
     }
 
 
-    function getPeoplebyHomeworld(req, res, mysql, context, complete){
-      var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.homeworld = ?";
-      console.log(req.params)
-      var inserts = [req.params.homeworld]
-      mysql.pool.query(query, inserts, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.people = results;
-            complete();
-        });
-    }
+    // function getPeoplebyHomeworld(req, res, mysql, context, complete){
+    //   var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.homeworld = ?";
+    //   console.log(req.params)
+    //   var inserts = [req.params.homeworld]
+    //   mysql.pool.query(query, inserts, function(error, results, fields){
+    //         if(error){
+    //             res.write(JSON.stringify(error));
+    //             res.end();
+    //         }
+    //         context.people = results;
+    //         complete();
+    //     });
+    // }
 
-    /* Find people whose fname starts with a given string in the req */
-    function getPeopleWithNameLike(req, res, mysql, context, complete) {
-      //sanitize the input as well as include the % character
-       var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.fname LIKE " + mysql.pool.escape(req.params.s + '%');
-      console.log(query)
+    // /* Find people whose fname starts with a given string in the req */
+    // function getPeopleWithNameLike(req, res, mysql, context, complete) {
+    //   //sanitize the input as well as include the % character
+    //    var query = "SELECT bsg_people.character_id as id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.planet_id WHERE bsg_people.fname LIKE " + mysql.pool.escape(req.params.s + '%');
+    //   console.log(query)
 
-      mysql.pool.query(query, function(error, results, fields){
-            if(error){
-                res.write(JSON.stringify(error));
-                res.end();
-            }
-            context.people = results;
-            complete();
-        });
-    }
+    //   mysql.pool.query(query, function(error, results, fields){
+    //         if(error){
+    //             res.write(JSON.stringify(error));
+    //             res.end();
+    //         }
+    //         context.people = results;
+    //         complete();
+    //     });
+    // }
 
 
 
@@ -77,7 +75,7 @@ module.exports = function(){
     router.get('/', function(req, res){
         var callbackCount = 0;
         var context = {};
-        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
+        context.jsscripts = ["deleteteam.js"];
         var mysql = req.app.get('mysql');
         getPeople(res, mysql, context, complete);
         getPlanets(res, mysql, context, complete);
@@ -90,43 +88,42 @@ module.exports = function(){
         }
     });
 
-    // Display all people from a given homeworld. Requires web based javascript to delete users with AJAX
-    router.get('/filter/:homeworld', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
-        var mysql = req.app.get('mysql');
-        getPeoplebyHomeworld(req,res, mysql, context, complete);
-        getPlanets(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 2){
-                res.render('people', context);
-            }
+    // // Display all people from a given homeworld. Requires web based javascript to delete users with AJAX
+    // router.get('/filter/:homeworld', function(req, res){
+    //     var callbackCount = 0;
+    //     var context = {};
+    //     context.jsscripts = ["deleteteam.js","filterpeople.js","searchpeople.js"];
+    //     var mysql = req.app.get('mysql');
+    //     getPeoplebyHomeworld(req,res, mysql, context, complete);
+    //     getPlanets(res, mysql, context, complete);
+    //     function complete(){
+    //         callbackCount++;
+    //         if(callbackCount >= 2){
+    //             res.render('people', context);
+    //         }
 
-        }
-    });
+    //     }
+    // });
 
 
 
-    // Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX
-    router.get('/search/:s', function(req, res){
-        var callbackCount = 0;
-        var context = {};
-        context.jsscripts = ["deleteperson.js","filterpeople.js","searchpeople.js"];
-        var mysql = req.app.get('mysql');
-        getPeopleWithNameLike(req, res, mysql, context, complete);
-        getPlanets(res, mysql, context, complete);
-        function complete(){
-            callbackCount++;
-            if(callbackCount >= 2){
-                res.render('people', context);
-            }
-        }
-    });
+    // // Display all people whose name starts with a given string. Requires web based javascript to delete users with AJAX
+    // router.get('/search/:s', function(req, res){
+    //     var callbackCount = 0;
+    //     var context = {};
+    //     context.jsscripts = ["deleteteam.js","filterpeople.js","searchpeople.js"];
+    //     var mysql = req.app.get('mysql');
+    //     getPeopleWithNameLike(req, res, mysql, context, complete);
+    //     getPlanets(res, mysql, context, complete);
+    //     function complete(){
+    //         callbackCount++;
+    //         if(callbackCount >= 2){
+    //             res.render('people', context);
+    //         }
+    //     }
+    // });
 
      // Display one person for the specific purpose of updating people
-
 
         router.get('/:id', function(req, res){
         callbackCount = 0;
@@ -187,7 +184,7 @@ module.exports = function(){
 
     router.delete('/:id', function(req, res){
         var mysql = req.app.get('mysql');
-        var sql = "DELETE FROM bsg_people WHERE character_id = ?";
+        var sql = "DELETE FROM fantasy_teams WHERE team_id = ?";
         var inserts = [req.params.id];
         sql = mysql.pool.query(sql, inserts, function(error, results, fields){
             if(error){
