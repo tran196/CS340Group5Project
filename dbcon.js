ab08389390b155794989ab79f6e87f1d4d4fdc59
Andres Arizala <arizala13@gmail.com>
2019-06-12 22:27:53 -0500

    Delete dbcon.js

--- dbcon.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var pool = mysql.createPool({
-  connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_arizalan',
-  password        : 'Joanna1313!',
-  database        : 'cs340_arizalan'
-});
-module.exports.pool = pool;
